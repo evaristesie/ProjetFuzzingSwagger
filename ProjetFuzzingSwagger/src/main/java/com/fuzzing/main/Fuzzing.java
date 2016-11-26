533a720897b2f4ed395441bbba3f14274da24682
LouisDable <Dablelouisqc@gmail.com>
2016-11-26 22:23:39 +0100

    Insertion du swagger.json et affichage du BasePath(exemple)

--- ProjetFuzzingSwagger/src/main/java/com/fuzzing/main/Fuzzing.java
+++ ProjetFuzzingSwagger/src/main/java/com/fuzzing/main/Fuzzing.java
@@ -1,10 +1,14 @@
 package com.fuzzing.main;
 
+import io.swagger.parser.SwaggerParser;
+import io.swagger.models.Swagger;
+
 public class Fuzzing {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Lecture du swagger.json
+		Swagger swagger = new SwaggerParser().read("./src/swagger.json");
+		System.out.println("Le Base Path est : "+swagger.getBasePath());
 	}
 
 }
