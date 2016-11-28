8b441d7e94f8903d0b2afb6fed0c4a3fd222a027
evariste <evariste.sie@gmail.com>
2016-11-28 02:09:47 +0100

    implementation du cas de test Parameter

--- ProjetFuzzingSwagger/src/main/java/com/fuzzing/Oracle/OracleCasTestParametre.java
+++ ProjetFuzzingSwagger/src/main/java/com/fuzzing/Oracle/OracleCasTestParametre.java
@@ -1,5 +1,35 @@
 package com.fuzzing.Oracle;
 
-public class OracleCasTestParametre {
+import org.apache.http.client.methods.CloseableHttpResponse;
 
+import com.fuzzing.casTest.CasTestParametre;
+
+public class OracleCasTestParametre {
+	
+	public int good=0,bad=0;
+	
+	public void OracleVerifParametre(CloseableHttpResponse rep,int codestatut,int a)
+	{
+		if(rep.getStatusLine().getStatusCode()==codestatut){
+			bad++;
+		  System.out.println("Bad Parameter");
+	}
+		else
+		{
+			good++;
+			System.out.println("Good Parameter");
+		}
+		
+		
+		    if(a==99)
+		     {
+		    	 System.out.println("Statistique des Données génerées ");
+					System.out.println("----------------------------------");
+					System.out.println("Sur 100 paramètres générés de façon aléatoire:");
+					System.out.println("Good parameter="+good);
+					System.out.println("Bad parameter="+bad);
+		
+		     }
 }
+}
+
--- ProjetFuzzingSwagger/src/main/java/com/fuzzing/main/Fuzzing.java
+++ ProjetFuzzingSwagger/src/main/java/com/fuzzing/main/Fuzzing.java
@@ -5,6 +5,7 @@ import io.swagger.parser.SwaggerParser;
 import java.io.Closeable;
 import java.io.IOException;
 import java.util.Map;
+import java.util.Random;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
@@ -14,32 +15,15 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 import io.swagger.models.Swagger;
-
+import com.fuzzing.casTest.CasTestParametre;
 public class Fuzzing {
 	
 	public static void main(String[] args) {
 		// Lecture du swagger.json
+		
 		Swagger swagger = new SwaggerParser().read("./src/swagger.json");
-		
-		//System.out.println("Resultat : "+swagger.getPath("/products"));
-		/*
-		 * Manipulation HttpClient
-		try {
-			int i;
-			for(i=0;i<1000000000;i++){
-				//Création d'un client Http
-				CloseableHttpClient httpclient = HttpClients.createDefault();
-				HttpGet httpGet = new HttpGet("http://localhost:8080/v1/swagger-ui.html");
-				CloseableHttpResponse response = httpclient.execute(httpGet);
-				System.out.println( (i+1) +":"+response.getStatusLine());
-				response.close();
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-		*/
-		
+		CasTestParametre t = new CasTestParametre();
+		t.VerifParametre();
 		
 	}
 
