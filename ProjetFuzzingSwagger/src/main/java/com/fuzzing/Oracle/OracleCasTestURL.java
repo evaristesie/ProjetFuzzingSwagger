a4c88e054dd91d0d5f1be275a7e169c2391f61de
LouisDable <Dablelouisqc@gmail.com>
2016-12-07 14:31:35 +0100

    check ok

--- ProjetFuzzingSwagger/src/main/java/com/fuzzing/Oracle/OracleCasTestURL.java
+++ ProjetFuzzingSwagger/src/main/java/com/fuzzing/Oracle/OracleCasTestURL.java
@@ -1,5 +1,31 @@
 package com.fuzzing.Oracle;
 
-public class OracleCasTestURL {
+import org.apache.http.client.methods.CloseableHttpResponse;
 
+public class OracleCasTestURL {
+	int good;
+	int bad;
+	public OracleCasTestURL() {
+		good = 0;
+		bad = 0;
+	}
+	
+	public void executeOracle(CloseableHttpResponse rep, int codeRep, int nb){
+		System.out.print("Reponse : "+rep.getStatusLine());
+		if( rep.getStatusLine().getStatusCode() == codeRep){
+			System.out.println(" Bad URL ");
+			bad++;
+		}else{
+			System.out.println(" Good URL ");
+			good++;
+		}
+		System.out.println();
+		if( (bad + good) == nb ){
+			System.out.println();
+			System.out.println("=====================================");
+			System.out.println(" Bilan sur "+nb+" Requests executées");
+			System.out.println(" Good URL : " + good);
+			System.out.println(" Bad URL : " + bad);
+		}
+	}
 }
--- ProjetFuzzingSwagger/src/main/java/com/fuzzing/main/Fuzzing.java
+++ ProjetFuzzingSwagger/src/main/java/com/fuzzing/main/Fuzzing.java
@@ -2,18 +2,7 @@ package com.fuzzing.main;
 
 import io.swagger.parser.SwaggerParser;
 
-import java.io.Closeable;
-import java.io.IOException;
-import java.util.Map;
-import java.util.Random;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-
+import com.fuzzing.casTest.CasTestURL;
 import io.swagger.models.Swagger;
 import com.fuzzing.casTest.CasTestParametre;
 public class Fuzzing {
@@ -22,9 +11,11 @@ public class Fuzzing {
 		// Lecture du swagger.json
 		
 		Swagger swagger = new SwaggerParser().read("./src/swagger.json");
+		CasTestURL testURL = new CasTestURL(swagger);
+		int nbData = 100;
+		testURL.startTest(nbData);		
 		CasTestParametre t = new CasTestParametre();
 		t.VerifParametre();
-		
 	}
 
 }
